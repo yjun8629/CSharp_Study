a05b01231238f1cd2b70a120b979a5948c296c4a
Jo Yongjun <yongjun5835@gmail.com>
2023-08-17 10:46:25 +0900

    d

---
+++ Guess_the_number/Guess_the_number/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Guess_the_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
