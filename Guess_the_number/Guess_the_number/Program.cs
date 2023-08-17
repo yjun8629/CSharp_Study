e9999599f8d4fe6cf560aac1b09cf7b6bf792954
Jo Yongjun <yongjun5835@gmail.com>
2023-08-17 11:05:30 +0900

    good

--- Guess_the_number/Guess_the_number/Program.cs
+++ Guess_the_number/Guess_the_number/Program.cs
@@ -4,8 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            int playerNum = 0;
+            int num = new Random().Next(1, 101);
+
+            while (playerNum != num)
+            {
+                Console.Write("1에서 100사이의 숫자를 입력해주세요.: ");
+                playerNum = int.Parse(Console.ReadLine());
+
+                if (playerNum < num)
+                {
+                    Console.WriteLine("더 큰 숫자를 입력해주세요!");
+                }
+                else if (playerNum > num)
+                {
+                    Console.WriteLine("더 작은 숫자를 입력해주세요!");
+                }
+                else
+                {
+                    Console.WriteLine("맞추셨습니다!");
+                }
+            }
         }
     }
 }
