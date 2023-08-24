1b78500b3d4cf2d6b989a9bf0ce012b12fac6a6f
Jo Yongjun <yongjun5835@gmail.com>
2023-08-24 10:07:30 +0900

    few

---
+++ Alogorithm_Problem/Alogorithm_Problem/230824_01.cs
@@ -0,0 +1,25 @@
+﻿namespace Alogorithm_Problem
+{
+    internal class _230824_01
+    {
+        static void Main(string[] args)
+        {
+            string answer = Solution(7);
+            Console.WriteLine(answer);
+        }
+
+        public static string Solution(int num)
+        {
+            string answer = "";
+            if (num % 2 == 1)
+            {
+                answer = "Odd";
+            }
+            else
+            {
+                answer = "Even";
+            }
+            return answer;
+        }
+    }
+}
